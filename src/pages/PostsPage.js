5a99f703075c7b8bca1f5f6e5063764b42265b5b
Aditya Chavan <adityachavan677@gmail.com>
2020-08-09 19:53:00 +0530

    completed postsPage

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -3,13 +3,34 @@ import { connect } from "react-redux";
 
 // Bring in the asynchronous fetchPosts action
 import { fetchPosts } from "../actions/postsActions";
+import { Post } from "../components/Post";
 
-const PostsPage = () => {
+const PostsPage = ({ poststate, fetchPosts }) => {
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderPosts = () => {
+    if (poststate.loading) return <p>Loding ...</p>;
+    if (poststate.hasErrors) return <p>Oooh ! Something went Wrong!</p>;
+    return poststate.posts.map((post) => <Post key={post.id} post={post} />);
+  };
+
   return (
     <section>
       <h1>Posts</h1>
+      {renderPosts()}
     </section>
   );
 };
 
-export default PostsPage;
+// Map Redux State to react component props
+const mapStateToProps = (state) => ({
+  poststate: state,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchPosts: () => dispatch(fetchPosts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsPage);
